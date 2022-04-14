58de39474848b9cddb602c195baaa7a9bd656890
sraboni-bhuiyan <bhuiyansraboni@gmail.com>
2022-04-14 21:34:15 +0200

    service component added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import repair1 from '../../../images/repair1.png'
+import Service from '../Service/Service'
 
-const services = [
+/* const services = [
     {id: 1, name: 'oil change', price: 120, description: '', img: 'https://i.ibb.co/HtgGNFt/repair1.png'}
 ]
-
+ */
 const Services = () => {
+
+    const [services, setServices] = useState([])
+
+    useEffect(() => {
+        fetch('services.json')
+        .then(res => res.json())
+        .then(data => setServices(data))
+    }, [])
+
     return (
         <div>
-            <h2>servicess</h2>
+            <h2>Services: {services.length}</h2>
+            {
+                services.map(service => <Service
+                    key={service.id}
+                    service={service}
+                ></Service>)
+            }
         </div>
     );
 };
